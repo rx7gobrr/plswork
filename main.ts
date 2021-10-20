d495be214b3d4d041b700fe7e5c0514071c2a940
rx7gobrr <91339729+rx7gobrr@users.noreply.github.com>
2021-10-20 13:46:08 -0400

    deleted

--- main.ts
+++ main.ts
@@ -1,26 +1,7 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.setVelocity(50, 50)
+    let mySprite: Sprite = null
     mySprite.setVelocity(50, 50)
 })
-let mySprite: Sprite = null
-mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . 3 3 . . . 
-    . . . . . . . . . . 3 . 3 . . . 
-    . . . . . . . . . . . 3 . . . . 
-    . . . . . . . . . . 3 . . . . . 
-    . . . . . . . . . 3 3 . . . . . 
-    . . . . . . . . . . 3 3 3 . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
 let mySprite2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
