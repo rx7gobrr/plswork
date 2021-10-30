71b1983602c482cc38d65de7c6d4d95a4680f90b
ICANTAIM <91802789+ICANTAIM@users.noreply.github.com>
2021-10-30 14:29:00 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -28,47 +28,28 @@ function Players () {
 function Tutorial () {
     game.splash("Welcome to the Night Fight Tutorial")
     game.splash("To move left and right, press left and right")
-    game.splash("Player 2 uses A and D")
     game.splash("Press A (Z) to jump")
+    game.splash("If playing with 2 players,")
+    game.splash("P1: wasd + q = A e = B")
+    game.splash("P2: ijkl + i = A o = B")
 }
 let Player_2: Sprite = null
 let Player_1: Sprite = null
-let mySprite: Sprite = null
-let On_Homescreen = 0
-if (On_Homescreen == 0) {
+let Homescreen = true
+scene.setBackgroundImage(assets.image`Home Menu`)
+if (Homescreen == true) {
     if (controller.A.isPressed()) {
         let Mode_Select = 0
-        mySprite = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Transition)
         if (Mode_Select == 0) {
-        	
-        }
-        if (Mode_Select == 1) {
-            animation.runImageAnimation(
-            mySprite,
-            assets.animation`Fade to black`,
-            100,
-            false
-            )
             tiles.setTilemap(tilemap`Night Fight Tutorial`)
             Players()
             Tutorial()
+        }
+        if (Mode_Select == 1) {
+        	
+        }
+        if (Mode_Select == 2) {
+        	
         }
     }
 }
